14b9ebbd06e154a60060a3b2046ffbc81d5fc553
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2021-04-12 16:59:00 -0400

    trying to do inputs not working tho

--- Entity/player.js
+++ Entity/player.js
@@ -1,10 +1,12 @@
 // create the entity
 var Player = new Entity("Player");
 
+// add components to player
 Player.add_component(Position({x: 100, y:100}));
 Player.add_component(Color());
 Player.add_component(CircleSize({r: 10}));
-Player.add_component(Health({health: 10}))
+Player.add_component(Health());
+Player.add_component(UserControlled());
 
 // save the entity
 makeEntity(Player.id, Player);
