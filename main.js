632662fe223e8490e70052dbf63e72f96d48b9cf
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2021-04-13 09:49:21 -0400

    idk

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-// create the entity
-var myEntity = new Entity("Player");
-
-myEntity.add_component(Position({x: 10, y:10}));
-myEntity.add_component(Color());
-myEntity.add_component(CircleSize({r: 100}));
-myEntity.add_component(Health({health: 10}))
-
-// save the entity
-makeEntity(myEntity.id, myEntity);
-
 // output of entities
 console.log(entities);
 
