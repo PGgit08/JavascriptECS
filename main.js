88d2359d42c24ee2d6bcfdad5b6193dedc134bbf
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2021-04-13 10:18:10 -0400

    fixes

--- main.js
+++ main.js
@@ -1,18 +1,9 @@
-// create the entity
-var myEntity = new Entity("Player");
-
-myEntity.add_component(Position({x: 10, y:10}));
-myEntity.add_component(Color());
-myEntity.add_component(CircleSize({r: 100}));
-myEntity.add_component(Health({health: 10}))
-
-// save the entity
-makeEntity(myEntity.id, myEntity);
-
 // output of entities
 console.log(entities);
 
 function MainLoop(){
+    ClearBackground();
+
     for(var entityId in entities){
         // get current entity
         curEntity = entities[entityId];
@@ -64,6 +55,3 @@ function MainLoop(){
 
 // start loop and render to screen every 5 milliseconds(low framerate)
 const IntervalId = window.setInterval(MainLoop, 0);
-// while (true){
-//     MainLoop();
-// }
