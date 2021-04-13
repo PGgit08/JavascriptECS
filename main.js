3d5d7e192c020838c7981494f50d498bca974425
PGgit08 <60079012+PGgit08@users.noreply.github.com>
2021-04-13 10:13:34 -0400

    nothing is working

--- main.js
+++ main.js
@@ -1,3 +1,14 @@
+// create the entity
+var myEntity = new Entity("Player");
+
+myEntity.add_component(Position({x: 10, y:10}));
+myEntity.add_component(Color());
+myEntity.add_component(CircleSize({r: 100}));
+myEntity.add_component(Health({health: 10}))
+
+// save the entity
+makeEntity(myEntity.id, myEntity);
+
 // output of entities
 console.log(entities);
 
